fe616a7aff4a9c0abedc53bfc0082dd6a2e69613
Vaibhav <vaibhav@csimplifyit.com-at-838282089561>
2024-05-19 22:28:40 +0530

    added cookies and success failure flash

--- routes/listing.js
+++ routes/listing.js
@@ -30,6 +30,10 @@ router.get("/new", (req,res) => {
 router.get("/:id", wrapAsync(async(req,res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
     res.render("listings/show.ejs", { listing });
 }));
 
@@ -39,6 +43,7 @@ router.post("/",validateListing,wrapAsync(async(req,res) => {
     // const listing = req.body.listing;
     const newListing = new Listing(req.body.listing);
     await newListing.save();
+    req.flash("success","New Listing Created");
     res.redirect("/listings");
 }));
 
@@ -46,6 +51,10 @@ router.post("/",validateListing,wrapAsync(async(req,res) => {
 router.get("/:id/edit", wrapAsync(async(req,res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
     res.render("listings/edit.ejs", { listing });
 }));
 
@@ -56,6 +65,7 @@ router.put("/:id",validateListing,wrapAsync(async(req,res) => {
     // }
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    req.flash("success","Listing Updated!");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -64,6 +74,7 @@ router.delete("/:id", wrapAsync(async(req,res) => {
     let { id } = req.params;
     const deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success","Listing Deleted!");
     res.redirect("/listings");
 }));
 
--- routes/review.js
+++ routes/review.js
@@ -26,6 +26,7 @@ router.post("/",validateReview,wrapAsync(async(req,res) => {
     await newReview.save();
     await listing.save();
 
+    req.flash("success","New Review Created");
     res.redirect(`/listings/${listing._id}`);
 }));
 
@@ -36,6 +37,7 @@ router.delete("/:reviewId",wrapAsync(async(req,res) => {
     await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
     await Review.findByIdAndDelete(reviewId);
 
+    req.flash("success","Review Deleted!");
     res.redirect(`/listings/${id}`);
 }));
 
