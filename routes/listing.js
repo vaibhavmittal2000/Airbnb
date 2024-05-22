06116ef544908f04c73251df45e9ec1216a9606f
Vaibhav <vaibhav@csimplifyit.com-at-838282089561>
2024-05-22 20:14:43 +0530

    authorization for edit,delete and listing

--- routes/listing.js
+++ routes/listing.js
@@ -1,20 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-const expressError = require("../utils/expressError.js");
-const { listingSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
-const {isLoggedIn} = require("../middleware.js");
-
-const validateListing = (req,res,next) => {
-    let {error} = listingSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new expressError(400, errMsg);
-    }else{
-        next();
-    }
-}
+const {isLoggedIn,isOwner,validateListing} = require("../middleware.js");
 
 // Index Route
 router.get("/", wrapAsync(async(req,res) => {
@@ -50,7 +38,7 @@ router.post("/",isLoggedIn,validateListing,wrapAsync(async(req,res) => {
 }));
 
 // Edit Route
-router.get("/:id/edit",isLoggedIn,wrapAsync(async(req,res) => {
+router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync(async(req,res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     if(!listing){
@@ -61,7 +49,7 @@ router.get("/:id/edit",isLoggedIn,wrapAsync(async(req,res) => {
 }));
 
 // Update Route
-router.put("/:id",isLoggedIn,validateListing,wrapAsync(async(req,res) => {
+router.put("/:id",isLoggedIn,isOwner,validateListing,wrapAsync(async(req,res) => {
     // if(!req.body.listing){
     //     throw new expressError(400,"Send valid data for listing");
     // }
@@ -72,7 +60,7 @@ router.put("/:id",isLoggedIn,validateListing,wrapAsync(async(req,res) => {
 }));
 
 // Delete Route
-router.delete("/:id",isLoggedIn,wrapAsync(async(req,res) => {
+router.delete("/:id",isLoggedIn,isOwner,wrapAsync(async(req,res) => {
     let { id } = req.params;
     const deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
--- routes/review.js
+++ routes/review.js
@@ -1,20 +1,9 @@
 const express = require("express");
 const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
-const expressError = require("../utils/expressError.js");
-const { reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-
-const validateReview = (req,res,next) => {
-    let {error} = reviewSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new expressError(400, errMsg);
-    }else{
-        next();
-    }
-}
+const {validateReview} = require("../middleware.js");
 
 // Review POST Route
 router.post("/",validateReview,wrapAsync(async(req,res) => {
